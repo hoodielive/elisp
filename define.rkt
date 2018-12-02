b0b8af362a55d5a418dbc14cbf440938682022cd
hoodielive <hoodielive@gmail.com>
2018-12-02 16:23:52 -0500

    Further operations on square

--- define.rkt
+++ define.rkt
@@ -2,3 +2,15 @@
 (define (square x) (* x x))
 
 (square 10)
+
+;; further operations
+(square 21)
+(square (+ 2 5))
+(square (square 3))
+
+(+ (square x) (square y))
+
+;; sum-of-squares
+(define (sum-of-squares x y)
+  (+ (square x) (square y)))
+(sum-of-squares 3 4)
